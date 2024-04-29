6eebf6a3af4a0dabc7a39e2f0ccf0a8266597c42
Eldos <eldos.baktybekov@gmail.com>
2024-04-29 22:54:15 +0600

    :label: Add computed field in `DetailedOrder` model

--- src/models/detailed_orders.py
+++ src/models/detailed_orders.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+from pydantic import computed_field
+
 from models.partial_orders import PartialOrder
 
 __all__ = ('DetailedOrder',)
@@ -12,3 +14,7 @@ class DetailedOrder(PartialOrder):
     receipt_printed_at: datetime | None
     courier_name: str | None
     canceled_by_user_name: str | None
+
+    @computed_field
+    def has_printed_receipt(self) -> bool:
+        return self.receipt_printed_at is not None
