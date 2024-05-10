2822a00ad6b9ccb371d9d7185d89e510a02e86a7
Eldos <eldos.baktybekov@gmail.com>
2024-05-10 13:42:15 +0600

    :fire: Remove unused time utility

--- src/time_helpers.py
+++ src/time_helpers.py
@@ -4,11 +4,7 @@ from fast_depends import Depends
 
 from config import Config, get_config
 
-__all__ = ('get_now', 'get_yesterday_this_moment')
-
-
-def get_now(config: Config = Depends(get_config)) -> datetime:
-    return datetime.now(config.timezone)
+__all__ = ('get_yesterday_this_moment',)
 
 
 def get_yesterday_this_moment(config: Config = Depends(get_config)) -> datetime:
