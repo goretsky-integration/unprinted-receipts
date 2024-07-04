fd9812a1260fcbfeba60dcdd9c0bccb7d3655b38
Eldos <eldos.baktybekov@gmail.com>
2024-07-04 14:50:41 +0600

    :label: Update `Event`, `EventPayload` models
    - Change event type name.
    - Update event payload structure.

--- src/models/events.py
+++ src/models/events.py
@@ -1,18 +1,18 @@
-from pydantic import BaseModel, Field
+from uuid import UUID
 
-from enums import CountryCode
-from models import DetailedOrder
+from pydantic import BaseModel, Field, conlist
+
+from models.dodo_is import OrderWithoutPrintedReceipt
 
 __all__ = ('EventPayload', 'Event')
 
 
 class EventPayload(BaseModel):
     unit_name: str
-    orders: list[DetailedOrder]
-    country_code: CountryCode
+    orders: conlist(OrderWithoutPrintedReceipt, min_length=1)
 
 
 class Event(BaseModel):
-    unit_ids: list[int]
-    type: str = Field(default='CANCELED_ORDERS', frozen=True)
+    unit_ids: UUID
+    type: str = Field(default='UNPRINTED_RECEIPTS', frozen=True)
     payload: EventPayload
