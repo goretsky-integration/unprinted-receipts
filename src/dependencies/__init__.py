009886662bdcb22f43cd836e7eee1d93ce667bec
Eldos <eldos.baktybekov@gmail.com>
2024-07-04 14:47:59 +0600

    :fire: Remove unused dependencies

--- src/dependencies/__init__.py
+++ src/dependencies/__init__.py
@@ -1,4 +1,2 @@
-from .auth_credentials_context import *
 from .connections import *
-from .dodo_is_context import *
 from .http_clients import *
--- src/dependencies/connections.py
+++ src/dependencies/connections.py
@@ -1,21 +1,17 @@
 from fast_depends import Depends
 
-from connections import AuthCredentialsStorageConnection, UnitsStorageConnection
+from connections.auth_credentials_storage import (
+    AuthCredentialsStorageConnection,
+)
 from connections.dodo_is import DodoIsConnection
 from dependencies.http_clients import (
     get_auth_credentials_storage_http_client,
     get_dodo_is_http_client,
-    get_units_storage_http_client,
 )
-from new_types import (
-    AuthCredentialsStorageHttpClient,
-    DodoIsHttpClient,
-    UnitsStorageHttpClient,
-)
+from new_types import AuthCredentialsStorageHttpClient, DodoIsHttpClient
 
 __all__ = (
     'get_auth_credentials_storage_connection',
-    'get_units_storage_connection',
     'get_dodo_is_connection',
 )
 
@@ -27,15 +23,6 @@ async def get_auth_credentials_storage_connection(
         ),
 ) -> AuthCredentialsStorageConnection:
     yield AuthCredentialsStorageConnection(http_client)
-
-
-async def get_units_storage_connection(
-        http_client: UnitsStorageHttpClient = Depends(
-            get_units_storage_http_client,
-            use_cache=False,
-        ),
-) -> UnitsStorageConnection:
-    yield UnitsStorageConnection(http_client)
 
 
 async def get_dodo_is_connection(
