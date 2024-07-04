6906b43c45585861e3cd32da431b1441cad05203
Eldos <eldos.baktybekov@gmail.com>
2024-07-04 14:12:03 +0600

    :fire: Remove unused models

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,4 @@
 from .auth_credentials import *
-from .detailed_orders import *
+from .dodo_is import *
 from .events import *
-from .order_history_items import *
-from .partial_orders import *
 from .units import *
